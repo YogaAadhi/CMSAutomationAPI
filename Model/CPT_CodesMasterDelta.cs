a6cdb6512706738e319af9ce0af4534c8bf7b1d2
yogaaadhi <yoga@wbcsoftwarelab.com>
2026-03-10 17:21:38 +0530

    Delta Model

---
+++ Model/CPT_CodesMasterDelta.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CMSAutomationAPI.Model
+{
+    [Table("oce_delta", Schema = "public")]
+    public class CPT_CodesMasterDelta
+    {
+        [Required]
+        [Column("code")]
+        [MaxLength(20)]
+        public string CptCode { get; set; }
+
+        [Column("long_desc")]
+        public string? Long_Desc { get; set; }
+
+        [Column("medium_desc")]
+        public string? Medium_Desc { get; set; }
+
+        [Column("short_desc")]
+        public string? Short_Desc { get; set; }
+
+        [Column("code_type")]
+        public string? Code_Type { get; set; }
+
+        [Column("process_type")]
+        public string? Process_Type { get; set; }
+
+        [Column("eff_date")]
+        public DateTime? EffDate { get; set; }
+
+        [Column("term_date")]
+        public DateTime? TermDate { get; set; }
+
+        [Column("created_on")]
+        public DateTime? CreatedOn { get; set; }
+
+        [Column("status")]
+        public string? status { get; set; }
+
+        [Column("updated_on")]
+        public DateTime? UpdatedOn { get; set; }
+
+    }
+}
