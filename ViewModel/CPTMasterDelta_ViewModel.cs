42220fd6ed3bb6c50d3007ee479fd3f5c5325c43
yogaaadhi <yoga@wbcsoftwarelab.com>
2026-03-10 17:23:03 +0530

    CodeMaster_ViewModel

---
+++ ViewModel/CPTMasterDelta_ViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace CMSAutomationAPI.ViewModel
+{
+    public class CPTMasterDelta_ViewModel
+    {
+        public string CptCode { get; set; }
+        public string? Long_Desc { get; set; }
+        public string? Medium_Desc { get; set; }
+        public string? Short_Desc { get; set; }
+        public string? Code_Type { get; set; }
+        public string? Process_Type { get; set; }
+        public DateTime? EffDate { get; set; }
+        public DateTime? TermDate { get; set; }
+        public string? Status { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+
+    }
+}
