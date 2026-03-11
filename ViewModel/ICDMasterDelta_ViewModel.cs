9ab0f37e695dbf8a8a5ba543c14ac013873181a1
yogaaadhi <yoga@wbcsoftwarelab.com>
2026-03-11 16:48:17 +0530

    ICDMasterDelta_ViewModel Adding

---
+++ ViewModel/ICDMasterDelta_ViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace CMSAutomationAPI.ViewModel
+{
+    public class ICDMasterDelta_ViewModel
+    {
+        public string ICDCode { get; set; }
+        public string? Description { get; set; }        
+        public string? Type { get; set; }
+        public DateTime? EffDate { get; set; }
+        public DateTime? TermDate { get; set; }
+        public string? Status { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+
+    }
+}
